c4af1d9a2052320969bed208ad6305e4bfab5c77
impersilvatech <impersilvatech@gmail.com>
2026-05-14 22:27:56 +0100

    Update and rename OneSignalSDKWorker.js to OneSignalSDKWorker.js

---
+++ Loja/OneSignalSDKWorker.js
@@ -0,0 +1,4 @@
+// OneSignalSDKWorker.js
+// ⚠️ Este ficheiro DEVE estar na raiz do domínio
+// Importa o SDK v16 do OneSignal (tem de coincidir com a versão carregada no admin.html)
+importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
--- loja/OneSignalSDKWorker.js
+++
@@ -1,2 +0,0 @@
-// OneSignalSDKWorker.js
-importScripts('https://cdn.onesignal.com/sdks/OneSignalSDKWorker.js');
