759237d2fd101bfeeb63575acff0e68639a6af62
impersilvatech <impersilvatech@gmail.com>
2026-05-15 09:21:15 +0100

    Update OneSignalSDKWorker.js

--- Loja/OneSignalSDKWorker.js
+++ Loja/OneSignalSDKWorker.js
@@ -1,4 +1,9 @@
-// OneSignalSDKWorker.js
-// ⚠️ Este ficheiro DEVE estar na raiz do domínio
-// Importa o SDK v16 do OneSignal (tem de coincidir com a versão carregada no admin.html)
-importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
+<script src="https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js" defer></script>
+<script>
+  window.OneSignalDeferred = window.OneSignalDeferred || [];
+  OneSignalDeferred.push(async function(OneSignal) {
+    await OneSignal.init({
+      appId: "28d74020-c173-4107-915f-ae48ef166118",
+    });
+  });
+</script>
