8f4fef2f3621c2f8ac2df2599353b2243af16569
impersilvatech <impersilvatech@gmail.com>
2026-05-15 09:52:11 +0100

    Create OneSignalSDKWorker.js

---
+++ Loja/OneSignalSDKWorker.js
@@ -0,0 +1,9 @@
+<script src="https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js" defer></script>
+<script>
+  window.OneSignalDeferred = window.OneSignalDeferred || [];
+  OneSignalDeferred.push(async function(OneSignal) {
+    await OneSignal.init({
+      appId: "28d74020-c173-4107-915f-ae48ef166118",
+    });
+  });
+</script>
